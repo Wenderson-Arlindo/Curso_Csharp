848b02c892028fd5480f0bfef70f08e5537a0c11
Wenderson-Arlindo <wenderson250408@gmail.com>
2024-08-14 18:22:38 -0300

    Add files via upload

--- Aprendendo_chatgpt/Aula02/aula02.cs
+++ Aprendendo_chatgpt/Aula02/aula02.cs
@@ -1,1 +1,19 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declarando variáveis
+        int idade = 25;
+        float altura = 1.75f;
+        string nome = "João";
+        bool maiorDeIdade = true;
+
+        // Exibindo os valores armazenados nas variáveis
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Altura: " + altura);
+        Console.WriteLine("Maior de idade? " + maiorDeIdade);
+    }
+}
---
+++ Aprendendo_chatgpt/Aula02/ex003.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class ex003
+{
+    static public void Main ()
+    {
+        // Declarando a primeira pessoa
+        string nome_p1 = "Wallace";
+        int idade_p1 = 8;
+        float altura_p1 = 1.23f;
+        
+        Console.WriteLine("Pessoa 01");
+        Console.WriteLine("Nome: "+nome_p1);
+        Console.WriteLine("Idade: "+idade_p1);
+        Console.WriteLine("Altura: "+altura_p1);
+        Console.WriteLine("");
+        
+        // Declarando a segunda pessoa
+        string nome_p2 = "Paulo";
+        int idade_p2 = 15;
+        float altura_p2 = 1.53f;
+        
+        Console.WriteLine("Pessoa 02");
+        Console.WriteLine("Nome: "+nome_p2);
+        Console.WriteLine("Idade: "+idade_p2);
+        Console.WriteLine("Altura: "+altura_p2);
+        Console.WriteLine("");
+        
+        // Declarando a terceira pessoa
+        string nome_p3 = "Wenderson";
+        int idade_p3 = 16;
+        float altura_p3 = 1.63f;
+        
+        Console.WriteLine("Pessoa 03");
+        Console.WriteLine("Nome: "+nome_p3);
+        Console.WriteLine("Idade: "+idade_p3);
+        Console.WriteLine("Altura: "+altura_p3);
+    }
+
+}
